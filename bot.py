2fcc989d615f208ac30e3fec551719af268aff35
hmanhduc2k <hmanhduc2k@gmail.com>
2023-08-20 03:15:04 +0800

    first commit

---
+++ bot.py
@@ -0,0 +1,17 @@
+import telebot
+
+# Replace 'YOUR_TOKEN' with your actual bot token
+bot = telebot.TeleBot('6645802484:AAFMbc7vkzpGrPcUF2zd4NK_M1wOXtt4JNM')
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Welcome to your bot! Type /hello to get started.")
+
+@bot.message_handler(commands=['hello'])
+def send_hello(message):
+    bot.reply_to(message, "Hello! How can I assist you today?")
+
+# Add more message handlers and commands as needed
+
+# Polling loop to continuously check for new messages
+bot.polling()
