fe33108b1b23cbb8c3d5cd7b5bf17782e0621f51
Manh Duc <hmanhduc2k@gmail.com>
2023-08-21 16:38:17 +0800

    add more files

--- bot.py
+++ bot.py
@@ -69,7 +69,7 @@ def show_tasks(message):
     with open('data/tasks.csv', 'r') as csvfile:
         csvreader = csv.DictReader(csvfile)
         for row in csvreader:
-            print(row)
+            print(row['chat_id'], row['status'])
             if row['chat_id'] == message.chat.id and row['status'] == 'ongoing':
                 filtered.append(row)
                 
