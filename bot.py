f5563dce175587ac734dad063f9da3c3714ea7b9
Manh Duc <hmanhduc2k@gmail.com>
2023-08-21 19:04:20 +0800

    add more files

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ def add_tasks(message):
             )
     
     
-@bot.message.handler(commands=['huyen'])
+@bot.message_handler(commands=['huyen'])
 def reply_to_huyen(message):
     bot.reply_to(message, "Anh yêu em nhiều lắm Huyền ơi!!! Anh chỉ muốn hôn em và nắm tay em nhiều hơn nữa :)))")
     
