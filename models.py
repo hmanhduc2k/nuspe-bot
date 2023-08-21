e725352e12a1ea8436bfa0672ff4f957a4c80ef1
Manh Duc <hmanhduc2k@gmail.com>
2023-08-21 21:07:20 +0800

    add database

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import uuid
 
 from sqlalchemy import create_engine, Column, Integer, String, TIMESTAMP
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
@@ -15,7 +16,7 @@ Base = declarative_base()
 class Tasks(Base):
     __tablename__ = 'tasks'
     def __init__(self, chat_id, task_name, task_assignee, task_deadlines, task_remarks):
-        self.task_id = uuid.uuid4()
+        self.task_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
         self.chat_id = chat_id
         self.task_name = task_name
         self.task_assignee = task_assignee
