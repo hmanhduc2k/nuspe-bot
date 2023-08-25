e9acc0c26b08ae6394410e8c605e2b172d94f289
Manh Duc <hmanhduc2k@gmail.com>
2023-08-26 00:52:29 +0800

    jkdjfka

--- commands/add_task_module.py
+++ commands/add_task_module.py
@@ -19,10 +19,10 @@ from sqlalchemy import cast, Date, extract
 from sqlalchemy.sql.expression import and_, or_
 
 # @bot.message_handler(commands=['huyen'])
-def reply_to_huyen(message):
-    message.reply_text("Anh yêu em nhiều lắm Huyền ơi!!! Anh chỉ muốn hôn em và nắm tay em nhiều hơn nữa :)))")
+def reply_to_huyen(bot, message):
+    bot.reply_to(message, "Anh yêu em nhiều lắm Huyền ơi!!! Anh chỉ muốn hôn em và nắm tay em nhiều hơn nữa :)))")
     
 def attach(bot_instance):
     @bot_instance.message_handler(commands=['huyen'])
     def handle_start(message):
-        reply_to_huyen(message)
+        reply_to_huyen(bot_instance, message)
